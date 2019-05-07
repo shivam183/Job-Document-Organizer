6b12ba49e2808d946892493882bc83712245ab6d
shivam183 <soodshivam1831997@gmail.com>
2019-05-07 12:28:54 -0400

    Created Create Directory Method && Create Joblink text file

--- Job_Doc_Organizer/Job_Doc_Organizer.cs
+++ Job_Doc_Organizer/Job_Doc_Organizer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,36 @@ namespace Job_Doc_Organizer
 {
     public partial class Job_Doc_Organizer : Form
     {
+        string path;
         public Job_Doc_Organizer()
         {
             InitializeComponent();
         }
 
-     
+        private void Create_Directory()
+        {
+            string folderName = @"C:\Users\shivam\Desktop\JOBS";
+            path = Path.Combine(folderName, Txt_FolderName.Text);
+            Directory.CreateDirectory(path);
+        }
+
+        private void Create_JobLinkTxtFile()
+        {
+            string FileName = "JobLink.txt";
+            path = Path.Combine(path, FileName);
+
+            if(!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path))
+                {
+                    using (StreamWriter sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine(Txt_JobLink.Text);
+                    }
+                }
+            }
+
+        }
+
     }
 }
