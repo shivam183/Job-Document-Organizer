57b52d86a3777648c4a372a9431b746609eacfd9
shivam183 <soodshivam1831997@gmail.com>
2019-05-07 14:04:58 -0400

    Added Valdations

--- Job_Doc_Organizer/Job_Doc_Organizer.cs
+++ Job_Doc_Organizer/Job_Doc_Organizer.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Job_Doc_Organizer
 {
     public partial class Job_Doc_Organizer : Form
     {
+        string folderName = @"C:\Users\shivam\Desktop\JOBS";
+        string FileName = "JobLink.txt";
+        string Orignal_ResumePath = @"C:\Users\shivam\Desktop\Resume.docx";
+        string Orignal_CoverLetterPath = @"C:\Users\shivam\Desktop\Cover_Letter.docx";
         string path;
         public Job_Doc_Organizer()
         {
@@ -21,47 +18,117 @@ namespace Job_Doc_Organizer
 
         private void Create_Directory()
         {
-            
-            string folderName = @"C:\Users\shivam\Desktop\JOBS";
-            path = Path.Combine(folderName, Txt_FolderName.Text);
-            Directory.CreateDirectory(path);
+            try
+            {
+                if (string.IsNullOrEmpty(Txt_FolderName.Text) || string.IsNullOrWhiteSpace(Txt_FolderName.Text))
+                {
+                    MessageBox.Show("Folder Name can't be empty", "Empty Folder Name", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    path = Path.Combine(folderName, Txt_FolderName.Text);
+                    if (Directory.Exists(path))
+                    {
+                        MessageBox.Show("Directory Name Already Exists", "Duplicate Directory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Exception",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
         }
 
         private void Create_JobLinkTxtFile()
         {
-            string FileName = "JobLink.txt";
-            string newPath = Path.Combine(path, FileName);
-
-            if(!File.Exists(newPath))
+            try
             {
-                using (FileStream fs = File.Create(newPath))
+                if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))
                 {
-                    using (StreamWriter sw = new StreamWriter(fs))
+                    MessageBox.Show($"Cant Find Folder {Txt_FolderName.Text}", "Invalid Folder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+
+                    if (string.IsNullOrEmpty(Txt_JobLink.Text) || string.IsNullOrWhiteSpace(Txt_JobLink.Text))
                     {
-                        sw.WriteLine(Txt_JobLink.Text);
+                        var output = MessageBox.Show("Job link Textbox is Empty, Create Empty TextFile?", "Textbox Empty", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                        if (output == DialogResult.Yes)
+                        {
+                            string newPath = Path.Combine(path, FileName);
+
+                            if (!File.Exists(newPath))
+                            {
+                                using (FileStream fs = File.Create(newPath))
+                                {
+
+                                }
+                            }
+                        }
                     }
+                    else
+                    {
+                        string newPath = Path.Combine(path, FileName);
+
+                        if (!File.Exists(newPath))
+                        {
+                            using (FileStream fs = File.Create(newPath))
+                            {
+                                using (StreamWriter sw = new StreamWriter(fs))
+                                {
+                                    sw.WriteLine(Txt_JobLink.Text);
+                                }
+                            }
+                        }
+                    }
+
+
+
                 }
             }
-
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Exception",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
         }
 
         private void Copy_Resume()
         {
-            if(Cb_Resume.Checked)
+            if (Cb_Resume.Checked)
             {
-                string Orignal_Path = @"C:\Users\shivam\Desktop\Resume.docx";
-                string Resume_path = Path.Combine(path,Path.GetFileName(Orignal_Path));
-                File.Copy(Orignal_Path, Resume_path);
+                try
+                {
+                    string Resume_path = Path.Combine(path, Path.GetFileName(Orignal_ResumePath));
+                    File.Copy(Orignal_ResumePath, Resume_path);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
 
         private void Copy_CoverLetter()
         {
-            if(Cb_CoverLetter.Checked)
+            if (Cb_CoverLetter.Checked)
             {
-                string Orignal_Path = @"C:\Users\shivam\Desktop\Cover_Letter.docx";
-                string CL_path = Path.Combine(path, Path.GetFileName(Orignal_Path));
-                File.Copy(Orignal_Path, CL_path);
+                try
+                {
+                    string CL_path = Path.Combine(path, Path.GetFileName(Orignal_CoverLetterPath));
+                    File.Copy(Orignal_CoverLetterPath, CL_path);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+
             }
         }
 
